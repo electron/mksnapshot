180ba9bdd60e0ed6947e64d8f550c7073df64ffc
John Kleinschmidt <jkleinsc@electronjs.org>
2021-05-26 12:28:20 -0400

    chore: update mksnapshot to work with Apple Silicon

--- mksnapshot.js
+++ mksnapshot.js
@@ -88,7 +88,7 @@ if (fs.existsSync(argsFile)) {
 }
 
 const options = {
-  cwd: workingDir,
+  cwd: mksnapshotBinaryDir,
   env: process.env,
   stdio: 'inherit'
 }
@@ -107,13 +107,13 @@ if (args.includes('--help')) {
   process.exit(0)
 }
 
-fs.copyFileSync(path.join(workingDir, 'snapshot_blob.bin'),
+fs.copyFileSync(path.join(mksnapshotBinaryDir, 'snapshot_blob.bin'),
   path.join(outputDir, 'snapshot_blob.bin'))
 
 const v8ContextGenCommand = getBinaryPath('v8_context_snapshot_generator', mksnapshotBinaryDir)
 let v8ContextFile = 'v8_context_snapshot.bin'
 if (process.platform === 'darwin') {
-  const targetArch = process.env.npm_config_arch
+  const targetArch = process.env.npm_config_arch || process.arch
   if (targetArch === 'arm64') {
     v8ContextFile = 'v8_context_snapshot.arm64.bin'
   } else {
