76247990a3601e9268791a20ec84269f7571262b
David Sanders <dsanders11@ucsbalum.com>
2023-02-13 01:01:20 -0800

    build: do CFA releases (#84)
    * build: WIP support for CFA releases
    
    * build: confirm version updated before publish
    
    * build: run tests against tag version for release job
    
    * build: put version fallback behavior behind environment variable
    
    * build: enable release workflow
    
    * build: cleanup publish script

--- download-mksnapshot.js
+++ download-mksnapshot.js
@@ -5,15 +5,31 @@ const extractZip = require('extract-zip')
 const versionToDownload = require('./package').version
 let archToDownload = process.env.npm_config_arch
 
-if (process.arch.indexOf('arm') === 0 && process.platform !== 'darwin') {
-  console.log(`WARNING: mksnapshot does not run on ${process.arch}. Download 
-  https://github.com/electron/electron/releases/download/v${versionToDownload}/mksnapshot-v${versionToDownload}-${process.platform}-${process.arch}-x64.zip
-  on a x64 ${process.platform} OS to generate ${archToDownload} snapshots.`)
-  process.exit(1)
+// Refs https://github.com/electron/fiddle-core/blob/1ee2d2737f23fd1012917a249a9444b6db89f1d8/src/versions.ts#L47-L57
+function compareVersions (a, b) {
+  const l = a.compareMain(b)
+  if (l) return l
+  // Electron's approach is nightly -> other prerelease tags -> stable,
+  // so force `nightly` to sort before other prerelease tags.
+  const [prea] = a.prerelease
+  const [preb] = b.prerelease
+  if (prea === 'nightly' && preb !== 'nightly') return -1
+  if (prea !== 'nightly' && preb === 'nightly') return 1
+  return a.comparePre(b)
 }
 
-if (archToDownload && archToDownload.indexOf('arm') === 0 && process.platform !== 'darwin') {
-  archToDownload += '-x64'
+// Refs https://github.com/electron/fiddle-core/blob/1ee2d2737f23fd1012917a249a9444b6db89f1d8/src/versions.ts#L152-L160
+function getLatestStable (releases) {
+  const { parse: semverParse } = require('semver')
+  const semvers = releases.map(({ version }) => semverParse(version)).filter((sem) => Boolean(sem))
+  semvers.sort((a, b) => compareVersions(a, b))
+  let stable
+  for (const ver of semvers.values()) {
+    if (ver.prerelease.length === 0) {
+      stable = ver
+    }
+  }
+  return stable
 }
 
 function download (version) {
@@ -28,6 +44,29 @@ function download (version) {
 }
 
 async function attemptDownload (version) {
+  // Fall back to latest stable if there is not a stamped version, for tests
+  if (version === '0.0.0-development') {
+    if (!process.env.ELECTRON_MKSNAPSHOT_STABLE_FALLBACK) {
+      console.log('WARNING: mksnapshot in development needs the environment variable ELECTRON_MKSNAPSHOT_STABLE_FALLBACK set')
+      process.exit(1)
+    }
+
+    const fetch = require('node-fetch')
+    const releases = await fetch('https://releases.electronjs.org/releases.json').then(response => response.json())
+    version = getLatestStable(releases).version
+  }
+
+  if (process.arch.indexOf('arm') === 0 && process.platform !== 'darwin') {
+    console.log(`WARNING: mksnapshot does not run on ${process.arch}. Download 
+    https://github.com/electron/electron/releases/download/v${version}/mksnapshot-v${version}-${process.platform}-${process.arch}-x64.zip
+    on a x64 ${process.platform} OS to generate ${archToDownload} snapshots.`)
+    process.exit(1)
+  }
+
+  if (archToDownload && archToDownload.indexOf('arm') === 0 && process.platform !== 'darwin') {
+    archToDownload += '-x64'
+  }
+
   try {
     const targetFolder = path.join(__dirname, 'bin')
     const zipPath = await download(version)
